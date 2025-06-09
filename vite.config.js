e422fcaa211698ffcb46f474cb2d77e563e0a104
Safdar Hussain <safdarhussainmangnejo@gmail.com>
2025-06-09 15:07:58 +0200

    updated file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/your-repo-name/", // 👈 IMPORTANT: Match your GitHub repo name
+  base: "/remwaste-web-design/", // 👈 IMPORTANT: Match your GitHub repo name
   plugins: [react()],
 });
